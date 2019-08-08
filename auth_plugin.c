0d5c0fd36c4375f2886198bec000ea7b0cd99be5
bartremans <bart.remans@gmail.com>
2019-08-08 13:40:18 +0000

    adding ref counters

--- auth_plugin.c
+++ auth_plugin.c
@@ -35,6 +35,10 @@ int mosquitto_auth_security_init(void *user_data, struct mosquitto_opt *opts, in
 // --- ACL CHECK ---
 int mosquitto_auth_acl_check(void *user_data, int access, struct mosquitto *client, const struct mosquitto_acl_msg *msg){
 
+  if(myModule != NULL){
+    ACL_Function = PyObject_GetAttrString(myModule, (char*)"ACLcheck");
+  };
+
   if(ACL_Function != NULL){
 
     PyObject *topic = PyUnicode_FromString(msg->topic);
@@ -43,30 +47,42 @@ int mosquitto_auth_acl_check(void *user_data, int access, struct mosquitto *clie
     PyObject *acc = PyLong_FromLong(access);
     PyObject *myResult = PyObject_CallFunctionObjArgs(ACL_Function, topic, clientid, acc, un, NULL);
 
+    Py_DECREF(topic);
+    Py_DECREF(clientid);
+    Py_DECREF(un);
+    Py_DECREF(acc);
+
     if(myResult != NULL){
+      Py_DECREF(myResult);
+      Py_DECREF(ACL_Function);
       return MOSQ_ERR_SUCCESS;
     } else {
+      Py_DECREF(ACL_Function);
       return MOSQ_ERR_ACL_DENIED;
     };
 
   } else {
-    return MOSQ_ERR_PLUGIN_DEFER;
+    return MOSQ_ERR_ACL_DENIED;
   }
 };
 
 // --- USERNAME/PASSWORD CHECK ---
 int mosquitto_auth_unpwd_check(void *user_data, struct mosquitto *client, const char *username, const char *password){
-  // Username Password get checked when allow_anonymous is False in config.
-
+  // Username Password get checked when allow_anonymous is False in config
+ return MOSQ_ERR_SUCCESS;
+ /*
  if(UNPWD_Function != NULL){
     PyObject *un = PyUnicode_FromString(username);
     PyObject *pwd = PyUnicode_FromString(password);
     PyObject *unpwd_result = PyObject_CallFunctionObjArgs(UNPWD_Function, un, pwd, NULL);
     if(unpwd_result != NULL){
       return MOSQ_ERR_SUCCESS;
+    } else {
+      printf("failed");
+      return MOSQ_ERR_AUTH;
     };
   };
-
+ */
 };
 
 int mosquitto_auth_plugin_version(void){
